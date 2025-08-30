25eae86f9f368d1507fda22d9b195b485c635f8a
infofromca <zgofmontreal@gmail.com>
2025-08-30 13:21:05 -0400

    Move OrchardCore.Commerce.ContentFields

--- src/Modules/OrchardCore.Commerce.ContentFields/Constants/ResourceNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OrchardCore.Commerce.ContentFields.Constants;
-
-public static class ResourceNames
-{
-    public const string JQuery = "jQuery";
-
-    public const string CommerceRegions = nameof(CommerceRegions);
-}
