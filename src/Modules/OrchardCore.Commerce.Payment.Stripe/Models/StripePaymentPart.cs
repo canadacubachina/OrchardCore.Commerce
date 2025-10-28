8b89e8aaef542b2d2192d7b5c48b61d196d5e1be
infofromca <zgofmontreal@gmail.com>
2025-10-28 11:50:40 -0400

    Add CardBrand to StripePaymentPart

--- src/Modules/OrchardCore.Commerce.Payment.Stripe/Models/StripePaymentPart.cs
+++ src/Modules/OrchardCore.Commerce.Payment.Stripe/Models/StripePaymentPart.cs
@@ -7,4 +7,11 @@ public class StripePaymentPart : ContentPart
 {
     public TextField PaymentIntentId { get; set; } = new();
     public int RetryCounter { get; set; }
+
+    public TextField CardBrand { get; set; } = new();
+    public TextField CardLast4 { get; set; } = new();
+
+    public NumericField CardExpMonth { get; set; } = new();
+
+    public NumericField CardExpYear { get; set; } = new();
 }
